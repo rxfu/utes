258b20c0ec6c24568bb37521fbba4f509bc151d6
Stephen Fu <stephen.frx@gmail.com>
2020-03-28 23:34:57 +0800

    添加user seeder

---
+++ config/setting.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'base' => env('MIX_BASE_URL', '/'),
+    'name' => env('MIX_APP_NAME', 'Laradmin'),
+    'slug' => env('MIX_APP_SLUG', 'Laradmin'),
+    'keywords' => 'Laravel, Bootstrap, Admin template',
+    'description' => 'An admin boilerplate developed with Bootstrap and AdminLTE base on Laravel',
+    'author' => 'Fu Rongxin',
+    'powerby' => 'Fu Rongxin',
+    'copyright' => 'Laradmin Company',
+    'password' => '123456',
+
+    // 状态代码,
+    'code' => [
+        200001 => '登录成功',
+        200002 => '退出成功',
+        401001 => '用户名或密码错误',
+        401002 => '用户未认证',
+    ]
+];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->string('name');
+            $table->string('email')->unique()->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('last_login_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
