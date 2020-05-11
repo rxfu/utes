631748efa04c0698c7c268f2f955f62212b01a05
Stephen Fu <stephen.frx@gmail.com>
2020-05-12 01:29:47 +0800

    user表添加字段及优化程序结构

--- app/Listeners/LogSuccessfulLogin.php
+++ app/Listeners/LogSuccessfulLogin.php
@@ -4,6 +4,8 @@ namespace App\Listeners;
 
 use App\Traits\Flash;
 use App\Services\LogService;
+use App\Services\SettingService;
+use App\Services\UserService;
 
 class LogSuccessfulLogin
 {
@@ -11,15 +13,23 @@ class LogSuccessfulLogin
 
     private $_service;
 
+    private $_userService;
+
+    private $_settingService;
+
     /**
      * Create the event listener.
      *
      * @param \App\Services\LogService $service
+     * @param \App\Services\UserService $userService
+     * @param \App\Services\SettingService $settingService
      * @return void
      */
-    public function __construct(LogService $service)
+    public function __construct(LogService $service, UserService $userService, SettingService $settingService)
     {
         $this->_service = $service;
+        $this->_userService = $userService;
+        $this->_settingService = $settingService;
     }
 
     /**
@@ -30,6 +40,14 @@ class LogSuccessfulLogin
      */
     public function handle($event)
     {
+        $user = $event->user;
+
+        if (!$this->_userService->isSuperAdmin($user) && $this->_settingService->getSetting('maintenance')) {
+            return redirect()->route('maintenance');
+        } else {
+            $this->_userService->successLogin($user);
+        }
+
         $this->_service->log(200004, $event->user, 'login');
 
         $this->success(200004);
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique()->comment('用户名');
             $table->string('password')->comment('密码');
             $table->string('name')->comment('真实姓名');
+            $table->string('phone')->unique()->nullable()->comment('联系电话');
             $table->string('email')->unique()->nullable()->comment('电子邮箱');
             $table->timestamp('email_verified_at')->nullable()->comment('电子邮箱验证时间');
             $table->boolean('is_enable')->default(true)->comment('是否启用，0-未启用，1-启用');
