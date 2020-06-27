d12ccf94cb6c15542696652d0051444df2773b53
Stephen Fu <stephen.frx@gmail.com>
2020-06-28 01:31:48 +0800

    添加计算总成绩逻辑

--- app/Services/ScoreService.php
+++ app/Services/ScoreService.php
@@ -3,17 +3,29 @@
 namespace App\Services;
 
 use App\Repositories\ScoreRepository;
+use App\Repositories\SettingRepository;
 
 class ScoreService extends Service
 {
-    public function __construct(ScoreRepository $scores)
+    protected $settings;
+
+    public function __construct(ScoreRepository $scores, SettingRepository $settings)
     {
         $this->repository = $scores;
+        $this->settings = $settings;
     }
 
     public function getRank($type = null)
     {
+        $year = $this->settings->item('year')->value;
+
         if (is_null($type)) {
+            $scores = $this->repository->findBy([
+                'year' => $year,
+            ]);
+        } else {
+            switch ($type) {
+            }
         }
     }
 }
